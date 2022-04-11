06687fdac6078fbe7aecb490551d840501a3ae43
zzsnzmn <zzsnzmn>
2022-04-10 19:02:59 -0500

    Get the scaling thing working.

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
   "os"
   "log"
   _ "image/png"
+  //"math"
+  "image/color"
 
-  "github.com/disintegration/imaging"
+  //"github.com/disintegration/imaging"
   "github.com/kevin-cantwell/dotmatrix"
-  "github.com/nfnt/resize"
+  //"github.com/nfnt/resize"
 )
 
 func Encode(img image.Image) error {
@@ -54,8 +56,35 @@ func main() {
     log.Fatal(err)
   }
   //m := resize.Resize(128, 64, img, resize.NearestNeighbor)
-  m := resize.Resize(128, 64, imaging.Invert(img), resize.NearestNeighbor)
+  //img = imaging.Invert(img)
   //log.Println(fmt.Sprintf("%+v", img.Bounds()))
   //Encode(m)
+  topLeft := image.Point{0, 0}
+  bottomRight := image.Point{128, 64}
+  m := image.NewRGBA(image.Rectangle{topLeft, bottomRight})
+  a := 0
+  b := 0
+  for x := 0; x < 512; x = x + 4 {
+    for y := 0; y < 256; y = y + 4 {
+      b++
+      //fmt.Println(a, b)
+      //fmt.Println(img)
+      //r, g, b, a := img.At(x, y).RGBA()
+      red, _, blue, _ := img.At(x, y).RGBA()
+      if red == 2056 && blue == 2056   {
+      //fmt.Println(fmt.Sprintf("%v", alpha))
+        m.Set(a, b, color.White)
+      //} else {
+        //fmt.Println(fmt.Sprintf("%v", img.At(x, y)))
+        //m.Set(a, b, color.Gray16{0})
+      } else {
+      //fmt.Println(fmt.Sprintf("%v", alpha))
+        m.Set(a, b, color.Black)
+      }
+    }
+    a++
+    b = 0
+  }
+
   fmt.Print(String(m))
 }
